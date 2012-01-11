d56008db1021af1fc679cb14129ade9c81f3c0a8
Hans-Gunther Schmidt <hgs@cloudcontrol.de>
2012-01-11 11:54:54 +0100

    adding new LoggingHandler
    - writes, by default, logs to /tmp/<logfile>

---
+++ src/LogHandler.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright 2011 cloudControl GmbH
+
+    This is the LogHandler. This class provides easy means of logging to a file.
+
+    Bug request to: Hans-Gunther Schmidt (hgs@cloudcontrol.com)
+"""
+
+import logging
+
+class LogHandler(object):
+    log_levels   = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+
+    logger       = 'default'
+    log_format   = '%(asctime)-15s %(name)s %(levelname)s %(message)s'
+    log_dir      = '/tmp/'
+    log_filename = '{0}.log'.format(logger)
+
+
+    def __init__(self):
+        """
+            Initialize LogHandler
+        """
+        super(LogHandler, self).__init__()
+
+        self.log_formatter = logging.Formatter(self.log_format)
+        self.log_file      = '{0}/{1}'.format(self.log_dir, self.log_filename)
+        self.log_handler   = logging.FileHandler(self.log_file)
+        self.log_handler.setFormatter(self.log_formatter)
+
+
+    def get_logger(self, logger=None):
+        """
+            Create the logger object and return it back
+        """
+        if logger is None:
+            logger = self.logger
+
+        log = logging.getLogger(logger)
+        log.addHandler(self.log_handler)
+        log.setLevel(logging.DEBUG)
+
+        return log
